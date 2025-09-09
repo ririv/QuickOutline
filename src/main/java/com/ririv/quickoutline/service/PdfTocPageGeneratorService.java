26b0ef678d9ddd97a65fb753b12cab6f15979fd8
ri <riri.v@outlook.com>
2025-09-10 02:04:03 +0800

    refactor(pdf): simplify PdfTocPageGeneratorService initialization

--- src/main/java/com/ririv/quickoutline/service/PdfTocPageGeneratorService.java
+++ src/main/java/com/ririv/quickoutline/service/PdfTocPageGeneratorService.java
@@ -1,20 +1,15 @@
 package com.ririv.quickoutline.service;
 
-import com.google.inject.Inject;
 import com.ririv.quickoutline.model.Bookmark;
 import com.ririv.quickoutline.pdfProcess.TocPageGenerator;
+import com.ririv.quickoutline.pdfProcess.itextImpl.iTextTocPageGenerator;
 
 import java.io.IOException;
 import java.util.List;
 
 public class PdfTocPageGeneratorService {
 
-    private final TocPageGenerator tocPageGenerator;
-
-    @Inject
-    public PdfTocPageGeneratorService(TocPageGenerator tocPageGenerator) {
-        this.tocPageGenerator = tocPageGenerator;
-    }
+    private final TocPageGenerator tocPageGenerator = new iTextTocPageGenerator();
 
     public void createTocPage(String srcFilePath, String destFilePath, List<Bookmark> bookmarks) throws IOException {
         tocPageGenerator.generateAndInsertToc(srcFilePath, destFilePath, bookmarks);
